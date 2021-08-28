06949b91e0240fd8c10bedeebd29f33dff6c2a7f
fedir-prylutskyi <lWAJmN49Xdv6>
2021-08-28 07:44:16 +0300

    fixed

--- src/app/shared/components/background/background.component.ts
+++ src/app/shared/components/background/background.component.ts
@@ -1,16 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {DeviceService} from "../../services/device.service";
+import {Subject} from "rxjs";
+import {takeUntil} from "rxjs/operators";
 
 @Component({
   selector: 'app-background',
   templateUrl: './background.component.html',
   styleUrls: ['./background.component.scss']
 })
-export class BackgroundComponent implements OnInit {
+export class BackgroundComponent implements OnInit, OnDestroy {
+  private destroy$ = new Subject<void>();
   imageSrc = 'assets/images/background2.jpeg';
+  isMobile = false;
 
-  constructor() { }
+  constructor(private deviceService: DeviceService) {
+    this.deviceService.isMobile$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(value => this.isMobile = value);
+  }
 
   ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
 }
